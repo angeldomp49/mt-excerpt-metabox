8c7bd0d01b4f221d800a8849c3efd8be550dac12
angeldomp49 <angeldomp49@gmail.com>
2021-02-04 17:57:06 -0600

    the plugin works at this moment

--- mt-excerpt-metabox.php
+++ mt-excerpt-metabox.php
@@ -1,5 +1,14 @@
 <?php
-
+/*
+Plugin Name: Excerpt Metabox
+Plugin URI: https://makechtecnology.com/excerpt-metabox
+Description: para el extracto
+Author: Makech Tecnology
+Version: 1.0.0
+Author URI:https://makechtecnology.com
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
 add_action( 'add_meta_boxes', 'mtEmCreateMetabox' );
 
 function mtEmCreateMetabox(){
@@ -8,13 +17,17 @@ function mtEmCreateMetabox(){
     $textAreaFunc = 'mtEmCreateTextArea';
     $pageType = 'post';
     $priority = 'normal';
-    $paramsForCallback = 'nothing';
+    $paramsForCallback = 'high';
 
     add_meta_box( $idSelector, $title, $textAreaFunc, $pageType, $priority, $paramsForCallback );
 }
 
+function mtEmCreateTestMetabox(){
+    echo( 'hi there im here!!!!' );
+}
+
 function mtEmCreateTextArea( $post ){
-    $excerpt = get_post_meta( $post->ID, 'excerpt', true );
+    $excerpt = get_the_excerpt( $post );
     ?>
         <p>Put your custom excerpt</p>
         <textarea name="mtEmExcerptContent" id="mtEmExcerptContent" cols="30" rows="10">
@@ -27,8 +40,10 @@ add_action( 'save_post', 'mtEmSaveExcerpt' );
 
 function mtEmSaveExcerpt( $postID ){
     if( isset( $_POST['mtEmExcerptContent'] ) ){
-        $post = get_post( $postID );
-        $post->excerpt = $_POST['mtEmExcerptContent'];
-        wp_update_post( $post );
+        $mtEmPost = [
+            'ID' => $postID,
+            'post_excerpt' => $_POST['mtEmExcerptContent']
+        ]; 
+        wp_update_post( $mtEmPost );
     }
 }
